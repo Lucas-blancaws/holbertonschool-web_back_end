a0e035865a303cf45026462c52d19bf48367b898
Lucas-blancaws <lblancportier@gmail.com>
2025-11-19 01:08:10 +0100

    1/4

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Fonction floor avec annotations de type"""
+import math
+
+
+def floor(n: float) -> int:
+    """Return la partie entière d'un float"""
+    return math.floor(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Fonction to_str avec annotations type"""
+
+
+def to_str(n: float) -> str:
+    """Return la représentation en chaîne de caractères d'un float"""
+    return str(n)
