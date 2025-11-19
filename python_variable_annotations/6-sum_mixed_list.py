098fb46693f8650e4f641f153a42d5bae5a2c146
Lucas-blancaws <lblancportier@gmail.com>
2025-11-19 08:48:49 +0100

    6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Fonction sum_mixed_list avec annotations type"""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Return la somme d'une liste d'entiers et de floats"""
     return sum(mxd_lst)
