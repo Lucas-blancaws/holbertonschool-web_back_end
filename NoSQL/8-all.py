fd851e18e52ee41d0f244070f57c37841802b68a
Lucas-blancaws <lblancportier@gmail.com>
2025-12-04 10:38:14 +0100

    8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module that lists all documents in a MongoDB collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection
+    Returns:
+    List of all documents in the collection, or empty list if no documents
+    """
+    documents = list(mongo_collection.find())
+    return documents
